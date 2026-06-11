20726c25e44086b5afd90668494f584c2fee4fda
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 23:41:27 +0300

    Fix DevExtreme licensing import order

--- React/eslint.config.js
+++ React/eslint.config.js
@@ -28,6 +28,7 @@ export default [
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
+      'import/extensions': 'off',
       'react/react-in-jsx-scope': 'off',
       'import/no-extraneous-dependencies': ['error', {
         devDependencies: [
