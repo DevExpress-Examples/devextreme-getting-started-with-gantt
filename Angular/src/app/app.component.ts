e9ca912b0a6e02ff067cbe1c54082ccf7a380d12
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:40:01 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxGanttModule } from 'devextreme-angular';
 import {
   Service, Task, Dependency, Resource, ResourceAssignment,
@@ -9,6 +9,7 @@ import {
   imports: [DxGanttModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.Eager,
   providers: [Service],
 })
 export class AppComponent {
